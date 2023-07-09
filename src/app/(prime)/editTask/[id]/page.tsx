bda94e8acd85eab915849b03fe536081bde81e07
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-09 20:39:44 +0600

    done

--- src/app/(prime)/editTask/[id]/page.tsx
+++ src/app/(prime)/editTask/[id]/page.tsx
@@ -5,7 +5,7 @@ const page = ({ params }: any) => {
         return (
                 <div>
                         <div className="md:w-3/4 mx-auto p-8">
-                                <h1 className="text-center text-3xl font-semibold text-info mb-2">Add a new task</h1>
+                                <h1 className="text-center text-3xl font-semibold text-info mb-2">Edit the task</h1>
                                 <Form params={params}></Form>
                         </div>
                 </div>
