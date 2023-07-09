89b9aa76fe6af4716cc147d119a52a7e89c8efc4
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-09 12:40:38 +0600

    some bug fixed

--- src/app/(prime)/editTask/[id]/page.tsx
+++ src/app/(prime)/editTask/[id]/page.tsx
@@ -1,71 +1,14 @@
-'use client'
-import { editAndUpdate, getSingleTask } from '@/app/utils/actions';
 import React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form"
-import Swal from 'sweetalert2';
-
-type Inputs = {
-        title: string
-        description: string
-        status: string
-}
+import Form from './Form';
 
 const page = ({ params }: any) => {
-        const getTask = getSingleTask(params.id);
-
-        const {
-                register,
-                handleSubmit,
-                formState: { errors },
-        } = useForm<Inputs>()
-        const onSubmit: SubmitHandler<Inputs> = (data) => {
-                editAndUpdate(data);
-                Swal.fire(
-                        'Good job!',
-                        'You clicked the button!',
-                        'success'
-                )
-        }
-
         return (
-                <form onSubmit={handleSubmit(onSubmit)} className='mt-4'>
-                        <div>
-                                {errors.title?.type === "required" && (<p className='text-red-500'>required</p>)}
-                                <input
-                                        defaultValue={getTask?.title}
-                                        type="text"
-                                        placeholder="Type here"
-                                        className="input input-bordered input-info w-full block"
-                                        {...register("title", { required: true })}
-                                        aria-invalid={errors.title ? "true" : "false"}
-                                />
+                <div>
+                        <div className="md:w-3/4 mx-auto p-8">
+                                <h1 className="text-center text-3xl font-semibold text-info mb-2">Add a new task</h1>
+                                <Form params={params}></Form>
                         </div>
-
-                        <div className='my-4'>
-                                {errors.description?.type === "required" && (<p className='text-red-500'>required</p>)}
-                                <textarea
-                                        defaultValue={getTask?.description}
-                                        placeholder="Type here"
-                                        className="textarea textarea-info w-full block"
-                                        {...register("description", { required: true })}
-                                        aria-invalid={errors.description ? "true" : "false"}
-                                />
-                        </div>
-
-                        <div>
-                                <select {...register("status")} className="select select-bordered select-sm w-full max-w-xs">
-                                        <option value={getTask?.status} disabled hidden selected>{getTask?.status}</option>
-                                        <option value="To Do">To Do</option>
-                                        <option value="In Progress">In Progress</option>
-                                        <option value="Completed">Completed</option>
-                                </select>
-                        </div>
-
-                        <button className="btn btn-info w-full mt-4">
-                                <input type="submit" value="Confirm Change" />
-                        </button>
-
-                </form>
+                </div>
         );
 };
 
